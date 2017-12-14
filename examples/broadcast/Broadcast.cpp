c457ad3cc41025730137ab9c5408c007df748363
spetruzza <steve.petruzza@gmail.com>
2017-12-14 15:37:30 -0700

    simplify example TaskGraph using task function

--- examples/broadcast/Broadcast.cpp
+++ examples/broadcast/Broadcast.cpp
@@ -104,51 +104,14 @@ DataFlow::Task Broadcast::task(uint64_t gId) const
 
 std::vector<Task> Broadcast::localGraph(ShardId id, const TaskMap* task_map) const
 {
-  TaskId i;
-
   // First get all the ids we need
   std::vector<TaskId> ids = task_map->tasks(id);
-//  for (int i=0; i< ids.size(); i++) {
-//    std::cout << "tasks:: " << ids[i] << " id:: " << id <<"\n";
-//  }
 
-  // The create the required number of tasks
+  // Create the required number of tasks
   std::vector<DataFlow::Task> tasks(ids.size());
-  std::vector<DataFlow::Task>::iterator it;
-
-  //! Now assign all the task ids
-  for (i=0;i<ids.size();i++)
-    tasks[i].id(ids[i]);
-
-  std::vector<TaskId> incoming(1); // There will always be 1 incoming
-  std::vector<std::vector<TaskId> > outgoing(1); // and one output
-  outgoing[0].resize(mValence); // That goes to valence many other tasks
-
-  //! Now assign the callback functions as well as the incoming and outgoing
-  for (it=tasks.begin();it!=tasks.end();it++) {
-    // If this is a leaf
-    if (it->id() >= (size() - pow(mValence,mLevels)))
-      it->callback(1);
-    else { // If we are not the leaf
-      it->callback(0); // We are a relay task
-
-      // And we have valence many outputs
-      for (i=0;i<mValence;i++)
-        outgoing[0][i] = it->id()*mValence + i + 1;
-
-      it->outputs() = outgoing;
-    }
-
-    // If we are not the root we have one incoming
-    if (it->id() > 0) {
-      incoming[0] = (it->id()-1) / mValence;
-      it->incoming() = incoming;
-    }
-    else { // If we are the root we have one outside input
-      incoming[0] = TNULL;
-      it->incoming() = incoming;
-    }
-  }// end-for all tasks
+  for (int i=0; i< ids.size(); i++) {
+    tasks[i] = task(ids[i]);
+  }
 
   return tasks;
 }
--- examples/reduction/Reduction.cpp
+++ examples/reduction/Reduction.cpp
@@ -68,57 +68,14 @@ Reduction::Reduction(std::string config)
 
 std::vector<Task> Reduction::localGraph(ShardId id, const TaskMap* task_map) const
 {
-  TaskId i;
-
-  //fprintf(stderr, "LocalGraph %d \n",id);
-
   // First get all the ids we need
   std::vector<TaskId> ids = task_map->tasks(id);
 
-  // The create the required number of tasks
-  std::vector<Task> tasks(ids.size());
-  std::vector<Task>::iterator it;
-
-  //! Now assign all the task ids
-  for (i=0;i<ids.size();i++)
-    tasks[i].id(ids[i]);
-
-  std::vector<TaskId> incoming(mValence); // There will be at most valence many incoming
-  std::vector<std::vector<TaskId> > outgoing(1); // and one output
-  outgoing[0].resize(1); // With one target
-
-  //! Now assign the callback functions as well as the incoming and outgoing
-  for (it=tasks.begin();it!=tasks.end();it++) {
-    // First we assign the inputs
-
-    // If we are a leaf
-    if (it->id() >= (size() - leafCount())) {
-      incoming.resize(1);
-      incoming[0] = TNULL;
-
-      it->incoming() = incoming;
-    }
-    else { // If we are not a leaf
-
-      // We have valence many inputs
-      incoming.resize(mValence);
-      for (i=0;i<mValence;i++)
-        incoming[i] = it->id()*mValence + i + 1;
-
-      it->incoming() = incoming;
-    }
-
-    // Then we assign the outputs
-    if (it->id() != 0) {// If we are not the root
-      it->callback(1);
-
-      outgoing[0][0] = (it->id() - 1) / mValence;
-      it->outputs() = outgoing;
-    }
-    else {  //If we are the root
-      it->callback(2);
-    }
-  }// end-for all tasks
+  // Create the required number of tasks
+  std::vector<DataFlow::Task> tasks(ids.size());
+  for (int i=0; i< ids.size(); i++) {
+    tasks[i] = task(ids[i]);
+  }
 
   return tasks;
 }
